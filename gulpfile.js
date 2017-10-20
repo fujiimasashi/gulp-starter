d1d48ec7d575011f3c5de1fdcfa5968d4b7e34b7
fujiimasashi <fujii.gee@zcomwallet.com>
2017-10-20 11:04:15 +0900

    初回コミット

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var uglify = require("gulp-uglify");
+var browser = require("browser-sync");
+
+gulp.task("server", function() {
+    browser({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+gulp.task("js", function() {
+    gulp.src(["js/**/*.js","!js/min/**/*.js"])
+        .pipe(uglify())
+        .pipe(gulp.dest("./js/min"))
+        .pipe(browser.reload({stream:true}))
+});
+ 
+gulp.task("sass", function() {
+    gulp.src("sass/**/*scss")
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest("./css"))
+        .pipe(browser.reload({stream:true}))
+});
+ 
+gulp.task("default",['server'], function() {
+    gulp.watch(["js/**/*.js","!js/min/**/*.js"],["js"]);
+    gulp.watch("sass/**/*.scss",["sass"]);
+});
